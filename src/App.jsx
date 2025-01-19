82dfedf2bfda764ab35038c4ba05d55092c09f38
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-20 00:17:44 +0100

    integrate Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import About from "./sections/About";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto relative">
-      <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-    </main>
+    <>
+      <main className="max-w-7xl mx-auto relative">
+        <Navbar />
+        <Hero />
+        <About />
+        <Projects />
+      </main>
+      <Analytics />
+    </>
   );
 };
 
